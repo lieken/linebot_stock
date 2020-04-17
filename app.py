35758099c1b1d6e21071923ab53c76fb43d64b62
lieken <lieken1225@gmail.com>
2020-04-17 16:21:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def handle_message(event):
     
     if re.match('[0-9]{4}[<>][0-9]',usespeak): # 先判斷是否是使用者要用來存股票的
         mongodb.write_user_stock_fountion(stock=usespeak[0:4], bs=usespeak[4:5], price=usespeak[5:])
-        line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+Stock_Strategy2.Name_Stock(usespeak[0:4])+'已經儲存成功'))
+        line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+Stock_Strategy2.Name_Stock(stock)+'已經儲存成功'))
         return 0
 
     
