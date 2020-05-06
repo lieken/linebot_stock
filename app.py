72282cb2115f045ef7c65d679c5f7ba2c59837af
lieken <lieken1225@gmail.com>
2020-05-06 09:46:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ def handle_message(event):
             ]
         )
     )
-    line_bot_api.reply_message(event.reply_token, buttons_template)
+    line_bot_api.push_message(uid, buttons_template)
+
 
 
 if __name__ == '__main__':
