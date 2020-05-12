56e5021d72ce7474e5dc0a1597219f04ab2dd241
lieken <lieken1225@gmail.com>
2020-05-12 13:06:10 +0800

    Update Stock_Strategy2.py

--- Stock_Strategy2.py
+++ Stock_Strategy2.py
@@ -1,40 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 17 12:18:38 2020
-
-@author: zang
-"""
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 
-###股票的名字
-#stock='2002'
-#要抓取的網址
-#url = 'https://tw.stock.yahoo.com/q/q?s=' + stock 
-#請求網站
-#list_req = requests.get(url)
-#將整個網站的程式碼爬下來
-#soup = BeautifulSoup(list_req.content, "html.parser")
-#股票的名字
-#StockN='/q/bc?s='+stock
-#soup.find('a', {'href':StockN}).text
 timeS = datetime.datetime.strftime(datetime.datetime.now(),'%Y%m%d')
-
-##股票的漲幅交易量
-stock='2002'
-url2 ='https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date='+timeS+'&stockNo='+stock
-geturl2=pd.read_html(url2,encoding='utf-8',header=1)
-df = pd.concat(geturl2)
-dfL = len(df)-1
-#時間開盤收盤價差買賣量
-T = df.iloc[dfL][0]
-OP = df.iloc[dfL][3]    
-CP = df.iloc[dfL][6]
-Diff = df.iloc[dfL][7]
-Tv = df.iloc[dfL][8]
 
 
 def Name_Stock(stock):
