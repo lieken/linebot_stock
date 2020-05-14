49128ec56aef8b26bafc28a9253d5e3e3bf44100
lieken <lieken1225@gmail.com>
2020-05-14 19:23:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def handle_postback(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Data : Buy 代碼測試成功'))
     elif x[0] == "basic":
         StockCompany1 = Stock_Strategy2.Name_Stock(x[1])
-        StockCompany2 = Stock_Strategy2.Price_Stock(x[1])
+        StockCompany2 = Stock_Strategy2.Basic_Stock(x[1])
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Data : basic 代碼測試成功\n股票號碼為 : '+ x[1] + '\n\n' + StockCompany1 + '\n'+ StockCompany2 ))
         #line_bot_api.reply_message(event.reply_token, TextSendMessage(text = StockCompany1))
         #line_bot_api.reply_message(event.reply_token, TextSendMessage(text = StockCompany2))
