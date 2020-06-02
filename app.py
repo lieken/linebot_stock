d20cd45fbd44a783f7d9f851ac7ddb903ae2aed9
lieken <lieken1225@gmail.com>
2020-06-02 18:37:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
 from linebot.models import (
         TemplateSendMessage,CarouselTemplate,
         CarouselColumn,PostbackAction,MessageAction,URIAction,FlexSendMessage,
