f0a97eb2f2fb02f4bcb01654d50949fcd38d5a7f
lieken <lieken1225@gmail.com>
2020-06-07 15:46:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,9 @@ def handle_message(event):
         else :
             line_bot_api.push_message(uid, TextSendMessage(text='您輸入的並不是上市股票號碼'))
     
-    elif re.match('技術面 [0-9]{4} 資料',usespeak):
-        stock = Stock(usespeak[5:9])
+    elif re.match('技術面 [0-9]{4} 資料',usespeak=):
+        #usespeak=技術面 [0-9]{4} 資料
+        stock = Stock(usespeak[4:8])
         bfp = BestFourPoint(stock)
         stock = list(bfp.best_four_point())           # 綜合判斷
         if stock[0] :
